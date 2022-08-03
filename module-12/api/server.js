10cc722372e2fb9ce881be8f74c345adabd2c6f2
OleksiiKachan <okachan@nobul.com>
2022-08-03 12:34:10 -0400

    add activity

--- module-12/api/server.js
+++ module-12/api/server.js
@@ -35,7 +35,7 @@ const server = http.createServer(async (req, res) => {
     res.writeHead(code, { "Content-Type": "application/json" });
     res.end(data);
   } else if (basePath === `/api/lotr/movie` && req.method === "GET") {
-    const { data, code } = await lotr.chapters();
+    const { data, code } = await lotr.getAllMovies();
 
     res.writeHead(code, { "Content-Type": "application/json" });
     res.end(data);
