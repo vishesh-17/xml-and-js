3228e664f503703b7e9613c9ae9d555f0cca3bf8
Oleksii Kachan <okachan@nobul.com>
2023-02-21 20:34:04 -0500

    fix: add missing file

---
+++ module-07/filter.js
@@ -0,0 +1,29 @@
+const pilots = [
+  {
+    id: 2,
+    name: "Wedge Antilles",
+    faction: "Rebels",
+  },
+  {
+    id: 8,
+    name: "Ciena Ree",
+    faction: "Empire",
+  },
+  {
+    id: 40,
+    name: "Iden Versio",
+    faction: "Empire",
+  },
+  {
+    id: 66,
+    name: "Thane Kyrell",
+    faction: "Rebels",
+  },
+];
+
+console.log(`Source:`);
+console.log(pilots);
+
+const empire = pilots.filter((pilot) => pilot.faction === "Empire");
+
+console.log(empire);
